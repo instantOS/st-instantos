ec28ecf4b2ba7d09094676d30d9bf9493f9a6089
paperbenni <paperbenni@gmail.com>
2020-08-10 11:03:08 +0200

    another font change

--- config.def.h
+++ config.def.h
@@ -5,12 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-/* Spare fonts */
-static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
-};
+static char *font = "Fira Code Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 
 static int borderpx = 2;
 
