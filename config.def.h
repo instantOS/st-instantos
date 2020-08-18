d31cd84d549958b183c84a7fc9f6d5a08be85689
paperbenni <paperbenni@gmail.com>
2020-08-18 20:57:56 +0200

    add font2 config

--- config.def.h
+++ config.def.h
@@ -200,6 +200,7 @@ static uint forcemousemod = ShiftMask;
  */
 ResourcePref resources[] = {
 		{ "font",         STRING,  &font },
+		{ "font",         STRING,  &font2 },
 		{ "color0",       STRING,  &colorname[0] },
 		{ "color1",       STRING,  &colorname[1] },
 		{ "color2",       STRING,  &colorname[2] },
